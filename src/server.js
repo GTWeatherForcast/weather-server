582b3f33b4bc1be499cb9b9df6ad3b84bc843857
Varun Vudathu <varun.vudathu@gmail.com>
2023-09-26 23:15:53 -0400

    post example and mongodb fix

--- src/server.js
+++ src/server.js
@@ -4,6 +4,22 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://forecastweather:GTwebdev@cluster0.o2oa4an.mongodb.net/?retryWrites=true&w=majority";
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+try {
+  client.connect();
+  client.db("admin").command({ ping: 1 });
+  console.log("You successfully connected to MongoDB!");
+} finally {
+  client.close();
+}
 
 // create express app and configure it
 const app = express();
@@ -16,6 +32,11 @@ app.get("/", (req, res) => {
     res.json({ message: "WeatherForecast server." });
 });
 
+app.post("/api/v1/sign-in", (req, res) => {
+  console.log(req.body.message);
+  res.json({ message: "works" });
+});
+
 // routes
 require('./routes/auth.routes')(app);
 
@@ -23,28 +44,4 @@ require('./routes/auth.routes')(app);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    run().catch(console.dir);
 });
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
